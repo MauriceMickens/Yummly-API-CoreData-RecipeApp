4fe89fc73697a6be5029e718f05114e12603cf22
Maurice Mickens <mauricetmickens@gmail.com>
2015-02-21 12:28:22 -0500

    Initial Commit

---
+++ YumYum2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YumYum2
+//
+//  Created by PhantomDestroyer on 2/21/15.
+//  Copyright (c) 2015 Loud Skies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YumYum2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YumYum2
+//
+//  Created by PhantomDestroyer on 2/21/15.
+//  Copyright (c) 2015 Loud Skies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
